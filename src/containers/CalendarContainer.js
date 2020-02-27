b448f1b2023cdd7b6031f089788c4728af08af84
github username <github email address>
2020-02-27 13:01:22 -0500

    setting up calendar section

---
+++ src/containers/CalendarContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class CalendarContainer extends React.Component {
+
+    render() {
+
+        return(
+            
+            <div>
+                Calendar Container
+            </div>
+        )
+    }
+}
+
+
+export default CalendarContainer
